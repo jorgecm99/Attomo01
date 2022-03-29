65ab6fbc1c16bc65ab9fc9ea009597e7757146fc
Jorge Corcoles <jcorcoles86@gmail.com>
2022-03-29 10:55:06 +0200

    colors

--- src/Componentes/Core/Header.js
+++ src/Componentes/Core/Header.js
@@ -20,13 +20,13 @@ function Header () {
       <FontAwesomeIcon icon={faBars} style={{color: "white"}}/>
       </div>
       <div className='dropdown-content'>
-        <li><a target="_blank" href="https://bstadium.es/entradas-de-partidos/" rel="noopener noreferrer">Entradas de partidos</a></li>
-        <li><a target="_blank" href="https://bstadium.es/tours-originales/" rel="noopener noreferrer">Tours originales</a></li>
-        <li><a target="_blank" href="https://bstadium.es/experiencias-con-entradas/" rel="noopener noreferrer">Experiencias día de partido"</a></li>
-        <li><a target="_blank" href="https://bstadium.es/salta-al-campo/" rel="noopener noreferrer">Salta al campo</a></li>
-        <li><a target="_blank" href="https://bstadium.es/experiencias-virtuales/" rel="noopener noreferrer">Experiencias virtuales</a></li>
-        <li><a target="_blank" href="https://bstadium.es/sportbox/" rel="noopener noreferrer">Sportbox</a></li>
-        <li><a target="_blank" href="https://bstadium.es/mi-cuenta/" rel="noopener noreferrer">Iniciar sesión</a></li>
+        <li><a target="_blank" href="https://bstadium.es/entradas-de-partidos/" rel="noopener noreferrer" style={{color: "white"}}>Entradas de partidos</a></li>
+        <li><a target="_blank" href="https://bstadium.es/tours-originales/" rel="noopener noreferrer" style={{color: "white"}}>Tours originales</a></li>
+        <li><a target="_blank" href="https://bstadium.es/experiencias-con-entradas/" rel="noopener noreferrer" style={{color: "white"}}>Experiencias día de partido</a></li>
+        <li><a target="_blank" href="https://bstadium.es/salta-al-campo/" rel="noopener noreferrer" style={{color: "white"}}>Salta al campo</a></li>
+        <li><a target="_blank" href="https://bstadium.es/experiencias-virtuales/" rel="noopener noreferrer" style={{color: "white"}}>Experiencias virtuales</a></li>
+        <li><a target="_blank" href="https://bstadium.es/sportbox/" rel="noopener noreferrer" style={{color: "white"}}>Sportbox</a></li>
+        <li><a target="_blank" href="https://bstadium.es/mi-cuenta/" rel="noopener noreferrer" style={{color: "white"}}>Iniciar sesión</a></li>
       </div>
     
     </div>
@@ -39,7 +39,7 @@ function Header () {
     </div>
     
         <div className='cart-icon'>
-        <a target="_blank" href="https://bstadium.es/carrito/" rel="noopener noreferrer"><FontAwesomeIcon icon={faShoppingCart}/></a>               
+        <a target="_blank" href="https://bstadium.es/carrito/" rel="noopener noreferrer" style={{color: "white"}}><FontAwesomeIcon icon={faShoppingCart}/></a>               
         </div>     
 </header>
 
